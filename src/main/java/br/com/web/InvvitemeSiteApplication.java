d0903e7ab210ba67f684cfcb811c9287a11c2c6d
Marcelo Toyoda <marcelohtoy@gmail.com>
2016-05-26 00:19:42 -0300

    Estrutura Inicial

---
+++ src/main/java/br/com/web/InvvitemeSiteApplication.java
@@ -0,0 +1,12 @@
+package br.com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvvitemeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvvitemeSiteApplication.class, args);
+	}
+}
