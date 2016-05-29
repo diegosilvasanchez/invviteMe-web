4d26f18cbb14598791c1ffe1781ad5e272dae98a
Marcelo Toyoda <marcelohtoy@gmail.com>
2016-05-29 00:19:54 -0300

    Estrutura inicial do site

--- src/main/java/br/com/web/InvvitemeSiteApplication.java
+++ src/main/java/br/com/web/InvvitemeSiteApplication.java
@@ -2,8 +2,10 @@ package br.com.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class InvvitemeSiteApplication {
 
 	public static void main(String[] args) {
